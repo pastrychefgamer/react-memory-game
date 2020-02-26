6caaf89c720c7aeb91ffc0b9aa36ef8ef46eb44f
William Aleshire <Mine@Williams-iMac.domain>
2020-02-25 16:37:24 -0800

    adds navbar files

--- react-memory-game/src/components/Navbar/Navbar.jsx
+++ react-memory-game/src/components/Navbar/Navbar.jsx
@@ -1,1 +1,16 @@
 import React from 'react';
+import styles from './Navbar.module.css';
+
+const Navbar = (props) => {
+    return (
+        <nav>
+            <h1>MEMORY GAME</h1>
+            <ul>
+                <li>Login</li>
+                <li>Signup</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
