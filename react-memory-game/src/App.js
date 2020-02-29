25949775b9c397d749486a38b22e644d900867e1
William Aleshire <Mine@Williams-iMac.domain>
2020-02-29 10:42:57 -0800

    added state to app.js

--- react-memory-game/src/App.js
+++ react-memory-game/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar/Navbar';
@@ -9,27 +9,38 @@ import Login from './pages/Login/Login';
 import Signup from './pages/Signup/Signup';
 
 import './App.css';
+import userService from './utils/userService';
 
-function App() {
-  return (
-    <div className="App-outer-container">
-      <Navbar />
-      <div className="App-inner-container">
-        <Switch>
-          <Route exact path="/" render={props =>
-            <Home />
-          }/>
-          <Route exact path="/login" render={props =>
-          <Login />
-          }/>
-          <Route exact path="/signup" render={props =>
-          <Signup {...props} />
-          }/>
-        </Switch>
+class App extends Component {
+  state = {
+    user: userService.getUser()
+  }
+
+  handleSignupOrLogin = () => {
+    this.setState({ user: userService.getUser() })
+  }
+
+  render() {
+    return (
+      <div className="App-outer-container">
+        <Navbar />
+        <div className="App-inner-container">
+          <Switch>
+            <Route exact path="/" render={props =>
+              <Home />
+            }/>
+            <Route exact path="/login" render={props =>
+            <Login />
+            }/>
+            <Route exact path="/signup" render={props =>
+            <Signup {...props} />
+            }/>
+          </Switch>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
