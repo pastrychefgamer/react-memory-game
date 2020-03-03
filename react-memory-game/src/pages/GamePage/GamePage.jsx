4b87ad842b112c09fabeb8b66e3ac62487dbca86
William Aleshire <Mine@Williams-iMac.domain>
2020-03-03 15:06:57 -0800

    added image placeholders and with css

--- react-memory-game/src/pages/GamePage/GamePage.jsx
+++ react-memory-game/src/pages/GamePage/GamePage.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import GameBoard from '../.././components/Gameboard/GameBoard';
 import styles from './GamePage.module.css';
 
 const GamePage = (props) => {
     return (
-        <main className={styles.GamePage}>
-            <h1>Game Page</h1>
+        <main className={styles.main}>
+            <div>image will go here</div>
+            <GameBoard />
+            <div>image will go here</div>
         </main>
     )
 }
