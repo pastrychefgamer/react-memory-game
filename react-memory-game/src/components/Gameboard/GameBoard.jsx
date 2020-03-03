eedc66fa35d657534b8917b94c6e3e6f7d54545e
William Aleshire <Mine@Williams-iMac.domain>
2020-03-03 11:08:24 -0800

    created file/dir structure and boilerplate

---
+++ react-memory-game/src/components/Gameboard/GameBoard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './Gameboard.module.css';
+
+const GameBoard = (props) => (
+    <div className={styles.GameBoard}>
+        <h1>Game Board</h1>
+    </div>
+);
+
+export default GameBoard;
---
+++ react-memory-game/src/pages/GamePage/GamePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './GamePage.module.css';
+
+const GamePage = (props) => (
+    <div className={styles.GamePage}>
+        <h1>Game Page</h1>
+    </div>
+);
+
+export default GamePage;
