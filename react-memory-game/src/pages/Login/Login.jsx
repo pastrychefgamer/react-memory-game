97dfb707ef14c773645a2c3cc96cb3ce72f61f38
William Aleshire <Mine@Williams-iMac.domain>
2020-03-03 11:43:45 -0800

    commented out style link for now

--- react-memory-game/src/pages/Login/Login.jsx
+++ react-memory-game/src/pages/Login/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import LoginForm from '../../components/LoginForm/LoginForm';
-import styles from '../../pages/Login/Login.module.css';
+// import styles from '../../pages/Login/Login.module.css';
 
 const Login = (props) => {
     return (
