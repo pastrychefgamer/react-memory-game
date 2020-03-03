a1deb0e1c804c9856b2a12e0111cfc42a7bc18fb
William Aleshire <Mine@Williams-iMac.domain>
2020-03-03 15:07:17 -0800

    added image placeholders and with css

--- react-memory-game/src/pages/Login/Login.jsx
+++ react-memory-game/src/pages/Login/Login.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import LoginForm from '../../components/LoginForm/LoginForm';
-// import styles from '../../pages/Login/Login.module.css';
+import styles from '../../pages/Login/Login.module.css';
 
 const Login = (props) => {
     return (
-        <main>
-        <h1>Login</h1>
+        <main className={styles.main}>
+        <div>image will go here</div>
         <LoginForm {...props} />
+        <div>image will go here</div>
         </main>
     );
 }
