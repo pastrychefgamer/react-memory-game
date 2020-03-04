8977f8664f51f950c92a175a52c7c1303aa1e887
William Aleshire <Mine@Williams-iMac.domain>
2020-03-04 14:35:09 -0800

    created and integrated not functional

---
+++ react-memory-game/src/components/MatchSum/MatchSum.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './MatchSum.module.css';
+
+const MatchSum = (props) => (
+    <main 
+        className={styles.main}
+    >
+        My matches = ?
+    </main>
+)
+
+export default MatchSum;
--- react-memory-game/src/pages/GamePage/GamePage.jsx
+++ react-memory-game/src/pages/GamePage/GamePage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import GameBoard from '../.././components/Gameboard/GameBoard';
 import GameTimer from '../../components/GameTimer/GameTimer';
 import NewGameButton from '../../components/NewGameButton/NewGameButton';
+import MatchSum from '../../components/MatchSum/MatchSum';
 
 
 import styles from './GamePage.module.css';
@@ -10,12 +11,15 @@ const GamePage = (props) => {
     return (
         <main className={styles.main}>
             <GameBoard />
-            <GameTimer
-                elapsedTime={props.elapsedTime}
-                handleTimerUpdate={props.handleTimerUpdate}
-                isTiming={props.isTiming}
-            />
-            <NewGameButton />
+            <div>
+                <MatchSum />
+                <GameTimer
+                    elapsedTime={props.elapsedTime}
+                    handleTimerUpdate={props.handleTimerUpdate}
+                    isTiming={props.isTiming}
+                />
+                <NewGameButton />
+            </div>
         </main>
     )
 }
