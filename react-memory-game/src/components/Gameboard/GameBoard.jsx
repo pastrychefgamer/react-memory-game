a45dc7082502bd207ac465a3a05ac7d378cf5020
William Aleshire <Mine@Williams-iMac.domain>
2020-03-04 11:07:26 -0800

    injected card component

--- react-memory-game/src/components/Gameboard/GameBoard.jsx
+++ react-memory-game/src/components/Gameboard/GameBoard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { getChars } from '../../services/marvel-api';
+import Card from '../Card/Card';
 
 import styles from './GameBoard.module.css';
 let chars = getChars();
@@ -8,6 +9,7 @@ console.log(chars);
 const GameBoard = (props) => (
     <main className={styles.main}>
         <h1>Game Board</h1>
+        <Card />
     </main>
 );
 
