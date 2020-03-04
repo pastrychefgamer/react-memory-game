152714cb10a8ba37e49e673c5062ebc26d7dd464
William Aleshire <Mine@Williams-iMac.domain>
2020-03-04 11:06:56 -0800

    component made with basic css

---
+++ react-memory-game/src/components/Card/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Card.module.css';
+
+const Card = (props) => {
+    return (
+        <main className={styles.main}>
+            <p>I'm a card</p>
+        </main>
+    )
+}
+
+export default Card;
