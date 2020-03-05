9f0358a837c9cbb0a0f0177ff3d737d8616a2f38
William Aleshire <Mine@Williams-iMac.domain>
2020-03-05 15:18:14 -0800

    refactor of call to get chars

--- react-memory-game/src/services/marvel-api.js
+++ react-memory-game/src/services/marvel-api.js
@@ -1,19 +1,53 @@
+import underscore from 'underscore';
 const md5 = require('md5');
 const BASE_URL =  "http://gateway.marvel.com/";
 
-function getChars() {
-    const ts = new Date().getTime();
-    const pubby = "bf2c6e8a34dadf498e632240c2612e4d";
-    const privy = "ebb0bcda2b3f2aed63fd969f105d315053158d9d";
-    const hash = md5(ts + privy + pubby);
-    const charURI = "v1/public/characters";
+const ts = new Date().getTime();
+const pubby = "bf2c6e8a34dadf498e632240c2612e4d";
+const privy = "ebb0bcda2b3f2aed63fd969f105d315053158d9d";
+const hash = md5(ts + privy + pubby);
+const charURI = "v1/public/characters";
 
-    let path = `${BASE_URL}${charURI}?ts=${ts}&apikey=${pubby}&hash=${hash}`
-    console.log(hash);
-    console.log(path);
-    return fetch(path, {
+
+let path = `${BASE_URL}${charURI}?ts=${ts}&apikey=${pubby}&hash=${hash}&limit=10`
+
+async function getSglChar(id) {
+    const charID = "1011334";
+    let charPath = `${path}/${charID}`
+    return fetch(charPath, {
         mode: "cors"
-    }).then(response => response.json());
+    }).then(response => response.json())
 }
 
-export {getChars};
+
+
+async function getChars() {
+    return fetch(path, {
+        mode: "cors"
+    }).then(response => response.json())
+}
+
+
+function getCharFormattedArray(arr) {
+    const charArr = [];
+            arr.map(charSgl => {
+            let charObj = {
+                id: charSgl.id,
+                name: charSgl.name,
+                thumbnail: `${charSgl.thumbnail.path}/standard_medium.${charSgl.thumbnail.extension}`
+            }
+            charArr.push(charObj);
+            charArr.push(charObj);
+        });
+    return charArr;
+}
+
+function shuffle(arr) {
+    return underscore.shuffle(arr);
+}
+
+export default {
+   getCharFormattedArray,
+   getChars,
+   shuffle
+};
